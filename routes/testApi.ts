4f2457ba1f4b851ac93d715d5aed1c9bf1bb7206
kutselabskii <osutanfirst@gmail.com>
2023-01-28 23:00:06 +0400

    Switch to Go

--- routes/testApi.ts
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/", function(req: any, res: any, next: any) {
-    res.send("API is working properly");
-});
-
-module.exports = router
